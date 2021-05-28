fbc69411f68767355771060ded74f0a69e619a0e
andro <androistuk@gmail.com>
2021-05-28 18:42:24 +0200

    dropdown on hover with HTML,CSS and JS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+     
+
+        $('[data-trigger="dropdown"]').on('mouseenter', function() {
+            var submenu = $(this).parent().find('.submenu');
+            submenu.fadeIn(300);
+        
+
+        $('.profile-menu').on('mouseleave', function(){
+            submenu.fadeOut(300);
+        })
+    });
+}); 
